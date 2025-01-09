6e9edcf5ea1aea0951959a28db6b6c0da82f6645
Yogimuhmmadirshad13 <yogimuhammadirshad@gmail.com>
2025-01-10 01:26:11 +0700

    individualProject Weather Web App

--- public/wmoCode.js
+++ public/wmoCode.js
@@ -2,281 +2,281 @@ let code = {
 	"0":{
 		"day":{
 			"description":"Sunny",
-			"image":"http://openweathermap.org/img/wn/01d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838470.png"
 		},
 		"night":{
 			"description":"Clear",
-			"image":"http://openweathermap.org/img/wn/01n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/16116/16116384.png"
 		}
 	},
 	"1":{
 		"day":{
 			"description":"Mainly Sunny",
-			"image":"http://openweathermap.org/img/wn/01d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838470.png"
 		},
 		"night":{
 			"description":"Mainly Clear",
-			"image":"http://openweathermap.org/img/wn/01n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838470.png"
 		}
 	},
 	"2":{
 		"day":{
 			"description":"Partly Cloudy",
-			"image":"http://openweathermap.org/img/wn/02d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838434.png"
 		},
 		"night":{
 			"description":"Partly Cloudy",
-			"image":"http://openweathermap.org/img/wn/02n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838434.png"
 		}
 	},
 	"3":{
 		"day":{
 			"description":"Cloudy",
-			"image":"http://openweathermap.org/img/wn/03d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/7802/7802775.png"
 		},
 		"night":{
 			"description":"Cloudy",
-			"image":"http://openweathermap.org/img/wn/03n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/7802/7802775.png"
 		}
 	},
 	"45":{
 		"day":{
 			"description":"Foggy",
-			"image":"http://openweathermap.org/img/wn/50d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/17798/17798772.png"
 		},
 		"night":{
 			"description":"Foggy",
-			"image":"http://openweathermap.org/img/wn/50n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/17798/17798772.png"
 		}
 	},
 	"48":{
 		"day":{
 			"description":"Rime Fog",
-			"image":"http://openweathermap.org/img/wn/50d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/17798/17798772.png"
 		},
 		"night":{
 			"description":"Rime Fog",
-			"image":"http://openweathermap.org/img/wn/50n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/17798/17798772.png"
 		}
 	},
 	"51":{
 		"day":{
 			"description":"Light Drizzle",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		},
 		"night":{
 			"description":"Light Drizzle",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		}
 	},
 	"53":{
 		"day":{
 			"description":"Drizzle",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		},
 		"night":{
 			"description":"Drizzle",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		}
 	},
 	"55":{
 		"day":{
 			"description":"Heavy Drizzle",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14949/14949069.png"
 		},
 		"night":{
 			"description":"Heavy Drizzle",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14949/14949069.png"
 		}
 	},
 	"56":{
 		"day":{
 			"description":"Light Freezing Drizzle",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/1635/1635293.png"
 		},
 		"night":{
 			"description":"Light Freezing Drizzle",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/1635/1635293.png"
 		}
 	},
 	"57":{
 		"day":{
 			"description":"Freezing Drizzle",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/1635/1635293.png"
 		},
 		"night":{
 			"description":"Freezing Drizzle",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/1635/1635293.png"
 		}
 	},
 	"61":{
 		"day":{
 			"description":"Light Rain",
-			"image":"http://openweathermap.org/img/wn/10d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/4478/4478287.png"
 		},
 		"night":{
 			"description":"Light Rain",
-			"image":"http://openweathermap.org/img/wn/10n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/4478/4478287.png"
 		}
 	},
 	"63":{
 		"day":{
 			"description":"Rain",
-			"image":"http://openweathermap.org/img/wn/10d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/4478/4478287.png"
 		},
 		"night":{
 			"description":"Rain",
-			"image":"http://openweathermap.org/img/wn/10n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/4478/4478287.png"
 		}
 	},
 	"65":{
 		"day":{
 			"description":"Heavy Rain",
-			"image":"http://openweathermap.org/img/wn/10d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838430.png"
 		},
 		"night":{
 			"description":"Heavy Rain",
-			"image":"http://openweathermap.org/img/wn/10n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838430.png"
 		}
 	},
 	"66":{
 		"day":{
 			"description":"Light Freezing Rain",
-			"image":"http://openweathermap.org/img/wn/10d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838430.png"
 		},
 		"night":{
 			"description":"Light Freezing Rain",
-			"image":"http://openweathermap.org/img/wn/10n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838430.png"
 		}
 	},
 	"67":{
 		"day":{
 			"description":"Freezing Rain",
-			"image":"http://openweathermap.org/img/wn/10d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838430.png"
 		},
 		"night":{
 			"description":"Freezing Rain",
-			"image":"http://openweathermap.org/img/wn/10n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838430.png"
 		}
 	},
 	"71":{
 		"day":{
 			"description":"Light Snow",
-			"image":"http://openweathermap.org/img/wn/13d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		},
 		"night":{
 			"description":"Light Snow",
-			"image":"http://openweathermap.org/img/wn/13n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		}
 	},
 	"73":{
 		"day":{
 			"description":"Snow",
-			"image":"http://openweathermap.org/img/wn/13d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		},
 		"night":{
 			"description":"Snow",
-			"image":"http://openweathermap.org/img/wn/13n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		}
 	},
 	"75":{
 		"day":{
 			"description":"Heavy Snow",
-			"image":"http://openweathermap.org/img/wn/13d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		},
 		"night":{
 			"description":"Heavy Snow",
-			"image":"http://openweathermap.org/img/wn/13n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		}
 	},
 	"77":{
 		"day":{
 			"description":"Snow Grains",
-			"image":"http://openweathermap.org/img/wn/13d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		},
 		"night":{
 			"description":"Snow Grains",
-			"image":"http://openweathermap.org/img/wn/13n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		}
 	},
 	"80":{
 		"day":{
 			"description":"Light Showers",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		},
 		"night":{
 			"description":"Light Showers",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		}
 	},
 	"81":{
 		"day":{
 			"description":"Showers",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		},
 		"night":{
 			"description":"Showers",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
 		}
 	},
 	"82":{
 		"day":{
 			"description":"Heavy Showers",
-			"image":"http://openweathermap.org/img/wn/09d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14949/14949069.png"
 		},
 		"night":{
 			"description":"Heavy Showers",
-			"image":"http://openweathermap.org/img/wn/09n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14949/14949069.png"
 		}
 	},
 	"85":{
 		"day":{
 			"description":"Light Snow Showers",
-			"image":"http://openweathermap.org/img/wn/13d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		},
 		"night":{
 			"description":"Light Snow Showers",
-			"image":"http://openweathermap.org/img/wn/13n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		}
 	},
 	"86":{
 		"day":{
 			"description":"Snow Showers",
-			"image":"http://openweathermap.org/img/wn/13d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		},
 		"night":{
 			"description":"Snow Showers",
-			"image":"http://openweathermap.org/img/wn/13n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/13370/13370664.png"
 		}
 	},
 	"95":{
 		"day":{
 			"description":"Thunderstorm",
-			"image":"http://openweathermap.org/img/wn/11d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838448.png"
 		},
 		"night":{
 			"description":"Thunderstorm",
-			"image":"http://openweathermap.org/img/wn/11n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838448.png"
 		}
 	},
 	"96":{
 		"day":{
 			"description":"Light Thunderstorms With Hail",
-			"image":"http://openweathermap.org/img/wn/11d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838448.png"
 		},
 		"night":{
 			"description":"Light Thunderstorms With Hail",
-			"image":"http://openweathermap.org/img/wn/11n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838448.png"
 		}
 	},
 	"99":{
 		"day":{
 			"description":"Thunderstorm With Hail",
-			"image":"http://openweathermap.org/img/wn/11d@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838448.png"
 		},
 		"night":{
 			"description":"Thunderstorm With Hail",
-			"image":"http://openweathermap.org/img/wn/11n@2x.png"
+			"image":"https://cdn-icons-png.flaticon.com/128/14838/14838448.png"
 		}
 	}
 }
